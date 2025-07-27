a375b08a4b14b879de4f3f8e1a656f4852ec2997
Luis Schwab <luisschwab@protonmail.com>
2025-07-27 12:15:05 -0400

    feat: add secp256k1 support

--- src/labubu.rs
+++ src/labubu.rs
@@ -3,3 +3,22 @@
 //! Labubu module
 //!
 //! This module builds a `Labubu Asset` from a funded address generated in the user's browser.
+
+use secp256k1::{Keypair, SecretKey, SECP256K1};
+
+/// TODO(@luisschwab): remove later (sepc256k1 test)
+#[cfg(test)]
+mod tests {
+    use secp256k1::{rand::thread_rng, Keypair, SecretKey, SECP256K1};
+
+    #[test]
+    fn secp256k1() {
+        let mut rng = thread_rng();
+
+        let sk = SecretKey::new(&mut rng);
+        let keypair = Keypair::from_secret_key(SECP256K1, &sk);
+        let pk = keypair.public_key();
+        println!("{:?}", sk);
+        println!("{:?}", pk);
+    }
+}
